93e68fe7a827f2b36a60339e43b0b4a5d0eccb6b
phdafoe <formador@aula8PovMacNfor.local>
2017-04-07 22:05:30 +0200

    Ajustes iniciales _ 070417 _ iRulllo_ V39

---
+++ iRullo/IRDetailCustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  IRDetailCustomCell.swift
+//  iRullo
+//
+//  Created by formador on 7/4/17.
+//  Copyright © 2017 formador. All rights reserved.
+//
+
+import UIKit
+
+class IRDetailCustomCell: UICollectionViewCell {
+    
+}
